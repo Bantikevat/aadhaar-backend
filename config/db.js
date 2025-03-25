f87bd1f03760c67b7713c1f1b5fac819334c6861
BanttKevat <bantikevat199@gmail.com>
2025-03-25 15:28:10 +0530

    Co-authored-by: BanttKevat <bantikevat199@gmail.com>

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI ; // Fallback
-    await mongoose.connect(uri);
+    const dtas = process.env.MONGO_URI ; // Fallback
+    await mongoose.connect(dtas);
     console.log("✅ MongoDB Connected!");
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error.message);
